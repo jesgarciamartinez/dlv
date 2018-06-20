07556eb5755a0325d603147c9c8ee0ff23e338db
Jesús García Martínez <jesus.garcia@kairosds.com>
2018-06-20 10:43:53 +0200

    feat: add number to string coercion so a single number as key works

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export default function dlv(obj, key, def, p) {
 	p = 0;
-	key = key.map ? key : key.split('.');
+	key = key.map ? key : (''+key).split('.');
 	while (obj && p<key.length) obj = obj[key[p++]];
 	return (obj===undefined || p<key.length) ? def : obj;
 }
